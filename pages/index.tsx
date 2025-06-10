eb53565cd3ddab4213dadeaedeff8e1370eefa16
Victor Pontis <victor@pont.is>
2025-06-10 17:29:15 -0400

    Simplify masked div styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,14 +15,9 @@ const MaskedDivGood = () => {
           width: 40px;
           height: 40px;
           background: #4285f4;
-          border-radius: 50%;
           mask-image:
             url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1"><circle r="0.5" cx="0.5" cy="0.5"/></svg>'),
             url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1"><circle r="0.6" cx="1.1" cy="0.5"/></svg>');
-          mask-size: 100% 100%;
-          mask-repeat: no-repeat;
-          mask-position: center;
-          -webkit-mask-composite: source-out;
           mask-composite: subtract;
         }
       `}</style>
@@ -55,10 +50,6 @@ const MaskedDivBad = () => {
             HEAD_MARGIN_PERCENTAGE}" cx="${0.5 +
             CUTOUT_AVATAR_PERCENTAGE_VISIBLE +
             HEAD_MARGIN_PERCENTAGE}" cy="0.5"/></svg>');
-          mask-size: 100% 100%;
-          mask-repeat: no-repeat;
-          mask-position: center;
-          -webkit-mask-composite: source-out;
           mask-composite: subtract;
         }
       `}</style>
